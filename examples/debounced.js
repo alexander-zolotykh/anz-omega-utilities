bd87421b54adaa56e41584ae7fe46734c89b327b
Alex Zolotyh <alex.zolotyh@omega-r.com>
2020-11-13 13:10:07 +0300

    add examples

---
+++ examples/debounced.js
@@ -0,0 +1,19 @@
+import { debounce } from "../src/index.js"
+
+const debounced = debounce(function () {
+    console.log("debounced has been called")
+}, 5000);
+
+let intervalCalls = 0;
+
+console.log('started');
+
+const intervalId = setInterval(function () {
+    debounced();
+
+    intervalCalls++;
+
+    if (intervalCalls === 10) {
+        clearInterval(intervalId);
+    }
+}, 1000);
